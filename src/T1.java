f0a5102a84e8df42521d4bc944cf2d561f7415b3
Kamalakumar <N87435@launcher.int.abnamro.com>
2018-09-22 12:14:02 +0200

    Initial commit

---
+++ src/T1.java
@@ -0,0 +1,11 @@
+public class T1 {
+
+    public static void main(String[] args) {
+
+        String a1 = "Hello!. ";
+        String a2 = "This is Sam";
+        System.out.println(a1 + a2);
+
+    }
+}
+
